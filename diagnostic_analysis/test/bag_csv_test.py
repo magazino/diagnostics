a58d7efde06c660dc47c95831da022e49ae25f57
Markus Grimm <grimm@magazino.eu>
2019-03-26 21:02:38 +0100

    Removed binary mode when opening files

--- diagnostic_analysis/test/bag_csv_test.py
+++ diagnostic_analysis/test/bag_csv_test.py
@@ -97,7 +97,7 @@ class TestBagToCSV(unittest.TestCase):
     ##\brief Test that CSV file has correct data, number of lines
     def test_export(self):
         # Read CSV, count rows
-        input_reader = csv.reader(open(self.filename, 'rb'))
+        input_reader = csv.reader(open(self.filename, 'r'))
         count = -1
         for row in input_reader:
             if count == -1:
